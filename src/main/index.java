a642e61e718e722287919660831867e5969cc1df
dianjuar <dianjuar@gmail.com>
2015-02-09 05:22:42 -0430

    Conexion de la primera ventana lanzada al inciar la app que conecta la simulacion con los robots

--- src/main/index.java
+++ src/main/index.java
@@ -28,39 +28,57 @@ public class index extends javax.swing.JFrame {
     private void initComponents() {
 
         buttonGroup1 = new javax.swing.ButtonGroup();
-        jRadioButton1 = new javax.swing.JRadioButton();
-        jRadioButton2 = new javax.swing.JRadioButton();
+        radio_Robots = new javax.swing.JRadioButton();
+        radio_Simulacion = new javax.swing.JRadioButton();
         jButton1 = new javax.swing.JButton();
         jTabbedPane1 = new javax.swing.JTabbedPane();
+        jPanel2 = new javax.swing.JPanel();
         jPanel1 = new javax.swing.JPanel();
         jSplitPane1 = new javax.swing.JSplitPane();
         jLabel1 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
-        jPanel2 = new javax.swing.JPanel();
         Titulo = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        buttonGroup1.add(jRadioButton1);
-        jRadioButton1.setSelected(true);
-        jRadioButton1.setText("Simulacion");
-        jRadioButton1.addChangeListener(new javax.swing.event.ChangeListener() {
+        buttonGroup1.add(radio_Robots);
+        radio_Robots.setSelected(true);
+        radio_Robots.setText("Robots");
+
+        buttonGroup1.add(radio_Simulacion);
+        radio_Simulacion.setText("Simulacion");
+        radio_Simulacion.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
-                jRadioButton1StateChanged(evt);
+                radio_SimulacionStateChanged(evt);
             }
         });
 
-        buttonGroup1.add(jRadioButton2);
-        jRadioButton2.setText("Robots");
-
         jButton1.setText("Empezar");
+        jButton1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                jButton1MouseClicked(evt);
+            }
+        });
 
         jTabbedPane1.addChangeListener(new javax.swing.event.ChangeListener() {
             public void stateChanged(javax.swing.event.ChangeEvent evt) {
                 jTabbedPane1StateChanged(evt);
             }
         });
+
+        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
+        jPanel2.setLayout(jPanel2Layout);
+        jPanel2Layout.setHorizontalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 626, Short.MAX_VALUE)
+        );
+        jPanel2Layout.setVerticalGroup(
+            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 301, Short.MAX_VALUE)
+        );
+
+        jTabbedPane1.addTab("Robots", jPanel2);
 
         jSplitPane1.setDividerLocation(300);
         jSplitPane1.setEnabled(false);
@@ -94,19 +112,6 @@ public class index extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("Simulacion", jPanel1);
 
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 626, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 301, Short.MAX_VALUE)
-        );
-
-        jTabbedPane1.addTab("Robots", jPanel2);
-
         Titulo.setFont(new java.awt.Font("Droid Sans Mono", 1, 24)); // NOI18N
         Titulo.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         Titulo.setText("Optimización por Colonia de Hormigas (ACO)");
@@ -127,10 +132,10 @@ public class index extends javax.swing.JFrame {
                                 .addContainerGap()
                                 .addComponent(Titulo))
                             .addGroup(layout.createSequentialGroup()
-                                .addGap(203, 203, 203)
-                                .addComponent(jRadioButton1)
-                                .addGap(78, 78, 78)
-                                .addComponent(jRadioButton2)))
+                                .addGap(240, 240, 240)
+                                .addComponent(radio_Robots)
+                                .addGap(50, 50, 50)
+                                .addComponent(radio_Simulacion)))
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
             .addGroup(layout.createSequentialGroup()
@@ -142,12 +147,12 @@ public class index extends javax.swing.JFrame {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
                 .addComponent(Titulo)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jTabbedPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jRadioButton1)
-                    .addComponent(jRadioButton2))
+                    .addComponent(radio_Simulacion)
+                    .addComponent(radio_Robots))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 62, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -159,21 +164,30 @@ public class index extends javax.swing.JFrame {
     private void jTabbedPane1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jTabbedPane1StateChanged
         
         if(jTabbedPane1.getSelectedIndex() == 0)
-            jRadioButton1.setSelected(true);
+            radio_Robots.setSelected(true);
         else
-            jRadioButton2.setSelected(true);
+            radio_Simulacion.setSelected(true);
         
         
     }//GEN-LAST:event_jTabbedPane1StateChanged
 
-    private void jRadioButton1StateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_jRadioButton1StateChanged
+    private void radio_SimulacionStateChanged(javax.swing.event.ChangeEvent evt) {//GEN-FIRST:event_radio_SimulacionStateChanged
         
-        if(jRadioButton1.isSelected())
-            jTabbedPane1.setSelectedIndex(0);
-        else
+        if(radio_Simulacion.isSelected())
             jTabbedPane1.setSelectedIndex(1);
+        else
+            jTabbedPane1.setSelectedIndex(0);
         
-    }//GEN-LAST:event_jRadioButton1StateChanged
+    }//GEN-LAST:event_radio_SimulacionStateChanged
+
+    private void jButton1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton1MouseClicked
+       
+        this.dispose();
+        if(radio_Simulacion.isSelected())                 
+            new FormSimulacion().setVisible(true);        
+        else
+            new FormRobots().setVisible(true);
+    }//GEN-LAST:event_jButton1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -217,11 +231,11 @@ public class index extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
-    private javax.swing.JRadioButton jRadioButton1;
-    private javax.swing.JRadioButton jRadioButton2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JSplitPane jSplitPane1;
     private javax.swing.JTabbedPane jTabbedPane1;
     private javax.swing.JTextArea jTextArea1;
+    private javax.swing.JRadioButton radio_Robots;
+    private javax.swing.JRadioButton radio_Simulacion;
     // End of variables declaration//GEN-END:variables
 }
